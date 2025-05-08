34c13f2aba654ef09f678e78fcf7448bb56d5334
Ayokunle <kunlewingo@gmail.com>
2025-05-08 11:48:37 +0100

    new update

--- src/app/student-profile/student-profile.component.ts
+++ src/app/student-profile/student-profile.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-student-profile',
-  templateUrl: './student-profile.component.html'
+  templateUrl: './student-profile.component.html',
+  styleUrls: ['./student-profile.component.css']
 })
 export class StudentProfileComponent {
   studentName = 'John Doe';
   studentAge = 20;
-  profileImageUrl = 'https://www.oasislimousines.co.uk/rolls-royce-cullinan-black-badge-2.html';
+  profileImageUrl = 'https://th.bing.com/th/id/R.3418054b48a04d15df783a8fe8133b12?rik=irPhVo7JHp6ftA&pid=ImgRaw&r=0';
   isButtonDisabled = false;
   colspanValue = 2;
+ 
+  disableButton() {
+    this.isButtonDisabled = true;
+  }
+  
+  enableButton() {
+    this.isButtonDisabled = false;
+  }
 }
